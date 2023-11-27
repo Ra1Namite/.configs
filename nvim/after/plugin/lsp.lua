a4a8e9b7fd4dda218996532079aab319f107f196
Sanjaya Rai <rainamite@gmail.com>
2023-11-27 22:48:03 +0545

    configured formatter and linters

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,49 +1,159 @@
-
-
-local lsp_zero = require('lsp-zero')
-
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'pyright', 'eslint', 'lua-language-server'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "tsserver", "rust_analyzer", "pyright", "eslint", "lua_ls", "efm" },
+	handlers = {
+		lsp_zero.default_setup,
+		-- lua_ls = function()
+			-- local lua_opts = lsp_zero.nvim_lua_ls()
+			-- require("lspconfig").lua_ls.setup(lua_opts)
+		-- end,
+        lua_ls = require("lspconfig").lua_ls.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = { -- custom settings for lua 
+                Lua = {
+                    -- make the language server recognize "vim" global 
+                    diagnostics = {
+                        globals = { "vim"}
+
+
+                    },
+                    workspace = {
+                        -- make language server aware of runtime files 
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] =  true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        }
+                    }
+
+                }
+            }
+        }),
+
+		python = require("lspconfig").pyright.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				pyright = {
+					disableOrganizeImports = false,
+					analysis = {
+						useLibraryCodeForTypes = true,
+						autoSearchPaths = true,
+						diagnosticMode = "workspace",
+						autoImportCompletions = true,
+					},
+				},
+			},
+		}),
+
+		type_script = require("lspconfig").tsserver.setup({
+
+			on_attach = on_attach,
+			capabilities = capabilities,
+			filetypes = {
+				"typescript",
+			},
+			root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", ".git"),
+		}),
+
+		efm = require("lspconfig").efm.setup({
+			filetypes = { "lua", "python", "rust", "go", "javascript", "typescript", "bash", "html", "css", "c", "cpp" },
+
+			init_options = {
+				documentFormatting = true,
+				documentRangeFormatting = true,
+				hover = true,
+				documentSymbol = true,
+				codeAction = true,
+				completion = true,
+			},
+			settings = {
+
+				rootMarkers = { ".git/" },
+				languages = {
+					lua = {
+						require("efmls-configs.linters.luacheck"),
+						require("efmls-configs.formatters.stylua"),
+					},
+					python = {
+						require("efmls-configs.linters.ruff"),
+						require("efmls-configs.linters.mypy"),
+						require("efmls-configs.formatters.ruff"),
+                        require("efmls-configs.formatters.isort")
+					},
+					typescript = {
+						require("efmls-configs.linters.eslint_d"),
+						require("efmls-configs.formatters.prettier_d"),
+					},
+				},
+			},
+		}),
+	},
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+	},
+	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+})
+-- Format on save
+local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = lsp_fmt_group,
+	callback = function()
+		local efm = vim.lsp.get_active_clients({ name = "efm" })
+		if vim.tbl_isempty(efm) then
+			return
+		end
+		vim.lsp.buf.format({ name = "efm", async = true })
+	end,
 })
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -99,6 +99,11 @@ _G.packer_plugins = {
     path = "/home/sanjayarai/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
+  ["efmls-configs-nvim"] = {
+    loaded = true,
+    path = "/home/sanjayarai/.local/share/nvim/site/pack/packer/start/efmls-configs-nvim",
+    url = "https://github.com/creativenull/efmls-configs-nvim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/home/sanjayarai/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -123,6 +128,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/sanjayarai/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["nvim-autopairs"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
+    loaded = true,
+    path = "/home/sanjayarai/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
@@ -197,6 +208,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for solarized-osaka.nvim]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0 colorscheme solarized-osaka\bcmd\bvim\0", "config", "solarized-osaka.nvim")
 time([[Config for solarized-osaka.nvim]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
